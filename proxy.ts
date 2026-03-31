36a134e2fa3dea5786c4ddd6213fc5172408d9d7
José <clasesvirtualez96@gmail.com>
2026-03-30 20:04:15 -0500

    feat: prueba proxy

--- proxy.ts
+++ proxy.ts
@@ -1,4 +1,4 @@
-// este archivo es un middleware para la aplicación pero se llama proxy.ts por convencion de Next.js xd
+// este archivo es un middleware para la aplicación pero se llama proxy.ts por convencion de Next.js
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { AUTH_COOKIE_NAME } from "@/lib/auth-config";
